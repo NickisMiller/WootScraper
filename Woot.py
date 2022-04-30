85eb625d01336b433c0fdfb7d47c666aabee4525
CodyJHeiser <codyjheiser@gmail.com>
2022-04-30 01:41:36 -0500

    Better looking woot.py code

--- Woot.py
+++ Woot.py
@@ -13,6 +13,8 @@ search_amazon = False
 top_woot_items = 2
 
 woot_url = "https://www.woot.com/alldeals?ref=w_ngh_et_1"
+
+# Export file name
 file_name_save = "export_woot_bot_{}.csv".format(
     int(round(datetime.now().timestamp())))
 
@@ -23,10 +25,9 @@ driver.get(woot_url)
 final_page_xpath = "/html/body/div[2]/div/div/div/div[3]/div/div[6]/div/div"
 item_name_xpath = "/html/body/div[2]/section[1]/section[1]/article[1]/div[3]/header/h1"
 item_name_xpath_2 = "/html/body/div[2]/section[1]/section[1]/article[1]/div[4]/header/h1"
-amazon_rank_xpath = "/html/body/div[1]/div[2]/div[8]/div[23]/div/ul[1]/li/span/text()[1]"
-amazon_rank_xpath_2 = "/html/body/div[1]/div[3]/div[10]/div[19]/div[7]/div/div/div/div[1]/div/div/table/tbody/tr[6]/td/span/span[1]"
 
 # God I hate xpath - it's the only way...
+# Searches for the xpath until it finds it, will add some failsafe later
 while True:
     try:
         final_page_number = int(
@@ -37,9 +38,6 @@ while True:
 
 href_library = []
 woot_item_values = {}
-amazon_item_values = {}
-
-final_export_values = {}
 
 
 def Scrape_Page(page_number):
@@ -69,67 +67,13 @@ def Scrape_Page(page_number):
             break
 
 
-def Search_Amazon_Prices(asin, woot_price, woot_name, woot_href):
-    global amazon_item_values, amazon_rank_xpath, amazon_rank_xpath_2
-
-    amazon_url = "https://www.amazon.com/dp/{}".format(asin)
-
-    driver.get(amazon_url)
-    html_source = driver.page_source
-
-    try:
-        price = float(
-            re.sub("[^0-9^.]", "", html_source.split('displayPrice":"$')[1].split(",")[0]))
-    except IndexError:
-        price = "null"
-
-    try:
-        number_of_reviews = int(driver.find_elements_by_id(
-            "acrCustomerReviewText")[0].text.split(" ")[0].replace(",", ""))
-    except IndexError:
-        number_of_reviews = "null"
-
-    try:
-        review_rating = float(driver.find_elements_by_css_selector(
-            "span[data-hook='rating-out-of-text']")[0].text.split(" ")[0])
-    except IndexError:
-        review_rating = "null"
-
-    try:
-        rank = int(html_source.split("Best Sellers Rank")[1].split(
-            " (<")[0].split("#")[1].split(" ")[0].replace(",", ""))
-        category = html_source.split("Best Sellers Rank")[1].split(
-            " (<")[0].split(" in ")[1].split(" (")[0]
-    except IndexError:
-        rank = "null"
-        category = "null"
-
-    try:
-        sold_by = int(html_source.split(
-            "New (")[1].split(")")[0].replace(",", ""))
-    except IndexError:
-        sold_by = 'null'
-
-    amazon_item_values[asin] = {
-        "ASIN": asin,
-        "amazon_price": price,
-        "amazon_reviews": number_of_reviews,
-        "amazon_rating": review_rating,
-        "amazon_rank": rank,
-        "amazon_category": category,
-        "amazon_url": amazon_url,
-        "woot_price": woot_price,
-        "woot_name": woot_name,
-        "woot_url": woot_href
-    }
-
-
 def Get_Product_Info(href, link_progress):
-    global woot_item_values, item_name_xpath
+    global woot_item_values
 
     driver.get(href)
     html_source = driver.page_source
 
+    # Get the following informaion from woot
     try:
         item_asin = html_source.split("Asin\":\"")[1].split("\"")[0]
     except IndexError:
@@ -139,19 +83,20 @@ def Get_Product_Info(href, link_progress):
         item_price = driver.find_elements_by_class_name("price")[0].text
         item_price = float(item_price.replace(",", "").split("$")[-1])
     except:
+        # Possibly from the range of prices where you select from different options
         item_price = "null - unknown error"
 
     try:
         item_name = driver.find_element_by_xpath(item_name_xpath).text
     except NoSuchElementException:
         try:
+            # If there is a sale, it moves to a different xpath?
+            # We should find a better way to solve this issue
             item_name = driver.find_element_by_xpath(item_name_xpath_2).text
         except NoSuchElementException:
             item_name = "null"
 
-    if search_amazon:
-        Search_Amazon_Prices(item_asin, item_price, item_name, href)
-
+    # Put into a dictionary for pushing to csv file
     woot_item_values[item_asin] = {
         "ASIN": item_asin,
         "Price": item_price,
@@ -159,64 +104,44 @@ def Get_Product_Info(href, link_progress):
         "URL": href
     }
 
-    # Save to excel file as its running
-
-    if search_amazon:
-        if(link_progress % 50 == 0):
-            with open("temp-file.csv", 'w') as f:
-                write_head_once = True
-                for asin, values in amazon_item_values.items():
-                    w = csv.DictWriter(f, values.keys())
-                    if write_head_once:
-                        w.writeheader()
-                    write_head_once = False
-                    w.writerow(values)
-    else:
-        if(link_progress % 50 == 0):
-            with open("temp-file.csv", 'w') as f:
-                write_head_once = True
-                for asin, values in woot_item_values.items():
-                    w = csv.DictWriter(f, values.keys())
-                    if write_head_once:
-                        w.writeheader()
-                    write_head_once = False
-                    w.writerow(values)
-
-# Search_Amazon_Prices("B099P9G7Q5")
+    # Save to temp csv file as its running - saves every 50 items
+    if(link_progress % 50 == 0):
+        with open("temp-file.csv", 'w') as f:
+            write_head_once = True
+            for asin, values in woot_item_values.items():
+                w = csv.DictWriter(f, values.keys())
+                if write_head_once:
+                    w.writeheader()
+                write_head_once = False
+                w.writerow(values)
 
 
-# Final page number here - final_page_number
 if (top_woot_items >= final_page_number) or (top_woot_items == False):
     set_page_range = final_page_number
 else:
     set_page_range = top_woot_items
 
+# Start at index of 1, loop until page end or what is specified above
 for page in range(1, set_page_range):
     Scrape_Page(page)
 
+# CLI updates for how many links have been processed so far
+# Good for debugging
 total_links_processed = 0
 for href in href_library:
     Get_Product_Info(href, total_links_processed)
     total_links_processed += 1
     print(str(total_links_processed) + ": " + href)
 
+# Please god chrome, stop using all my ram
 driver.quit()
 
-if search_amazon:
-    with open(file_name_save, 'w') as f:
-        write_head_once = True
-        for asin, values in amazon_item_values.items():
-            w = csv.DictWriter(f, values.keys())
-            if write_head_once:
-                w.writeheader()
-            write_head_once = False
-            w.writerow(values)
-else:
-    with open(file_name_save, 'w') as f:
-        write_head_once = True
-        for asin, values in woot_item_values.items():
-            w = csv.DictWriter(f, values.keys())
-            if write_head_once:
-                w.writeheader()
-            write_head_once = False
-            w.writerow(values)
+# Write the dictionary to the CSV file
+with open(file_name_save, 'w') as f:
+    write_head_once = True
+    for asin, values in woot_item_values.items():
+        w = csv.DictWriter(f, values.keys())
+        if write_head_once:
+            w.writeheader()
+        write_head_once = False
+        w.writerow(values)
