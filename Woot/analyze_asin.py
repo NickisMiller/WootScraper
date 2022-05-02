9cb61008229cb9e00ed485d43c93ca47472b0db0
CodyJHeiser <codyjheiser@gmail.com>
2022-05-01 19:36:20 -0500

    updates

--- Woot/analyze_asin.py
+++ Woot/analyze_asin.py
@@ -40,19 +40,14 @@ remember_me_name = "rememberMe"
 select_storefront_class = "picker-name"
 submit_storefront_class = "picker-switch-accounts-button"
 
-asin_id = "katal-id-3"
-submit_asin_xpath = "/html/body/div/div[2]/div/kat-box/div/div[1]/form/kat-button//button"
+asin_cstm = "[unique-id='katal-id-3']"
+submit_asin_cstm = "kat-button[label='Search']"
 
-cost_goods_id = "katal-id-30"
-misc_cost_goods_id = "katal-id-31"
+cost_goods_cstm = "[unique-id='katal-id-30']"
+misc_cost_goods_cstm = "[unique-id='katal-id-31']"
 
 # Amazon collect information directs
-sales_rank_xpath = "/html/body/div/div[2]/div/kat-box/div/div[1]/div[3]/table/tbody/tr[3]/td[2]"
-offers_xpath = "/html/body/div/div[2]/div/kat-box/div/div[1]/div[3]/table/tbody/tr[4]/td[2]"
-total_reviews_xpath = "/html/body/div/div[2]/div/kat-box/div/div[1]/div[3]/table/tbody/tr[5]/td[2]/span/span"
-rating_custom_dom = "kat-star-rating"
-
-cpu_xpath = "/html/body/div/div[2]/div/div[2]/kat-box[1]/div[2]/div[2]/div/div[1]/div[1]/kat-label[2]//label/slot/span"
+cpu_xpath = "kat-label[text='Estimated cost per unit']"
 net_unit_profit_xpath = "/html/body/div/div[2]/div/div[2]/kat-box[1]/div[2]/div[2]/div/div[1]/div[2]/kat-label[2]//label/slot/span"
 amazon_fees_xpath = "/html/body/div/div[2]/div/div[2]/kat-box[1]/div[2]/div[1]/div[3]/div/kat-expander[1]/div[1]/kat-label//label/slot/span"
 
@@ -60,7 +55,7 @@ amazon_fees_xpath = "/html/body/div/div[2]/div/div[2]/kat-box[1]/div[2]/div[1]/d
 def Pull_ASIN(csv_file=main_csv_file):
     global asin_csv, price_csv
 
-    with open(main_csv_file, "r", newline="") as file:
+    with open(main_csv_file, "r", newline="", encoding="utf8") as file:
         reader = csv.reader(file, delimiter=",")
         for row in reader:
             # Skips the title row and puts the rest into lists
@@ -71,7 +66,22 @@ def Pull_ASIN(csv_file=main_csv_file):
 
 
 def Get_Amazon_Info(asin_list, price_list):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    # Slow down reCaptcha settings
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = webdriver.Chrome(
+        options=options, executable_path=ChromeDriverManager().install())
+
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+                           "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.execute_script(
+        "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
     driver.get(amazon_url)
 
     wait = WebDriverWait(driver, 10)
@@ -124,53 +134,72 @@ def Get_Amazon_Info(asin_list, price_list):
 
     for asin, woot_price in zip(asin_list, price_list):
         if asin != "null":
-            # Wait for data load
-            time.sleep(5)
-            asin_textbox = driver.find_element_by_id(asin_id)
-            submit_asin_button = driver.find_element_by_xpath(
-                submit_asin_xpath)
-
             while True:
                 try:
-                    asin_textbox = driver.find_element_by_id(asin_id)
-                    submit_asin_button = driver.find_element_by_xpath(
-                        submit_asin_xpath)
+                    asin_textbox = driver.find_element_by_css_selector(
+                        asin_cstm)
+
+                    submit_asin_button = driver.find_element_by_css_selector(
+                        submit_asin_cstm)
+
                     break
                 except NoSuchElementException:
-                    time.sleep(.1)
-
-            asin_textbox.clear()
+                    time.sleep(.25)
 
             asin_textbox.send_keys(asin)
             submit_asin_button.click()
 
-            # Wait for data load
             while True:
                 try:
-                    cog_textbox = driver.find_element_by_id(cost_goods_id)
-                    misc_cog_textbox = driver.find_element_by_id(
-                        misc_cost_goods_id)
+                    cog_textbox = driver.find_element_by_css_selector(
+                        cost_goods_cstm)
+                    misc_cog_textbox = driver.find_element_by_css_selector(
+                        misc_cost_goods_cstm)
                     break
                 except NoSuchElementException:
-                    time.sleep(.1)
-
-            cog_textbox.clear()
-            misc_cog_textbox.clear()
+                    time.sleep(.25)
 
             cog_textbox.send_keys(woot_price)
-            misc_cog_textbox.send_keys()
+            # misc_cog_textbox.send_keys()
 
             # Collect the Amazon info
-            sales_rank = driver.find_element_by_xpath(sales_rank_xpath).text
-            offers = driver.find_element_by_xpath(offers_xpath).text
-            total_reviews = driver.find_element_by_xpath(
-                total_reviews_xpath).text
+            matches = ["#", ",", "(", ")"]
+            matches_found = 0
 
-            rating = driver.find_element_by_css_selector(
-                rating_custom_dom).get_attribute("value")
-            print(rating)
+            for td in driver.find_elements_by_css_selector("td"):
+                if any(x in td.text for x in matches):
+                    if matches_found == 0:
+                        sales_rank = td.text
+                        print('sales: ' + str(matches_found))
+                    if matches_found == 1:
+                        total_reviews = td.text
+                        print('total reviews: ' + str(matches_found))
 
-            cpu = driver.find_element_by_xpath(cpu_xpath).text
+                    matches_found += 1
+                    print('match found: ' + td.text)
+
+                if " offers" in td.text:
+                    offers = td.text
+
+            print(sales_rank)
+            print(total_reviews)
+            time.sleep(1)
+
+            html_source = driver.page_source
+
+            Func = open("GFG-1.html", "w")
+
+            # Adding input data to the HTML file
+            Func.write(html_source)
+
+            # Saving the data into the HTML file
+            Func.close()
+
+            for foobar in driver.find_elements_by_xpath("kat-label"):
+                print(foobar.text)
+                print('-------')
+
+            cpu = driver.find_element_by_class_name(cpu_xpath).text
             net_unit_profit = driver.find_element_by_xpath(
                 net_unit_profit_xpath).text
             amazon_fees = driver.find_element_by_xpath(amazon_fees_xpath).text
